af7f7212efcb0405db4b08f2b4622753f9c0a77b
Ayan Mukherjee <ayanmukherjee20102011@gmail.com>
2021-06-10 11:48:10 +0530

    firebase initialized

---
+++ src/misc/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDlD8VffoD4Uc9Bkk2uGGCo9Bv0dQhJTBs',
+  authDomain: 'chat-web-app-649dc.firebaseapp.com',
+  databaseURL: 'https://chat-web-app-649dc-default-rtdb.firebaseio.com',
+  projectId: 'chat-web-app-649dc',
+  storageBucket: 'chat-web-app-649dc.appspot.com',
+  messagingSenderId: '628635135236',
+  appId: '1:628635135236:web:bd2286f653a2d860aff451',
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
