6cd89b3c814eb27f691208e667b91511ab78f1e1
Ayan Mukherjee <ayanmukherjee20102011@gmail.com>
2021-06-10 22:26:33 +0530

    profile data fetched. loader added.

--- src/context/profile.context.js
+++ src/context/profile.context.js
@@ -1,12 +1,57 @@
-import { React, createContext, useState, useContext } from 'react';
+import { React, createContext, useState, useContext, useEffect } from 'react';
+import { auth, database } from '../misc/firebase';
 
 const ProfileContext = createContext();
 
 export const ProfileProvider = ({ children }) => {
-  const [profile] = useState(false);
+  const [profile, setProfile] = useState(null);
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    let currentUserRef;
+
+    const authUnsub = auth.onAuthStateChanged(currentUser => {
+      if (currentUser) {
+        // console.log(currentUser);
+
+        currentUserRef = database.ref(`/profiles/${currentUser.uid}`);
+        currentUserRef.on('value', snapshot => {
+          const data = snapshot.val();
+          // console.log('snapshot', data);
+
+          const { name, createdAt } = data;
+
+          const currentUserData = {
+            name,
+            createdAt,
+            uid: currentUser.uid,
+            email: currentUser.email,
+          };
+
+          setProfile(currentUserData);
+          setLoading(false);
+        });
+      } else {
+        if (currentUserRef) {
+          currentUserRef.off();
+        }
+
+        setProfile(null);
+        setLoading(false);
+      }
+    });
+
+    return () => {
+      authUnsub();
+
+      if (currentUserRef) {
+        currentUserRef.off();
+      }
+    };
+  }, []);
 
   return (
-    <ProfileContext.Provider value={profile}>
+    <ProfileContext.Provider value={{ profile, isLoading }}>
       {children}
     </ProfileContext.Provider>
   );
