9a01c4e05690da870f0bd02c2e504665c9960939
Ayan Mukherjee <ayanmukherjee20102011@gmail.com>
2021-06-11 10:41:02 +0530

    drawer is made resposive

--- src/components/dashboard/DashboardToggle.js
+++ src/components/dashboard/DashboardToggle.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { Button, Drawer, Icon } from 'rsuite';
 import Dashboard from '.';
-import { useModalState } from '../../misc/custom-hooks';
+import { useMediaQuery, useModalState } from '../../misc/custom-hooks';
 
 const DashboardToggle = () => {
   const { isOpen, openModal, closeModal } = useModalState();
-
+  const isMobileDevice = useMediaQuery('(max-width: 992px)');
   return (
     <>
       <Button block color="blue" onClick={openModal}>
         <Icon icon="dashboard" /> Dashboard
       </Button>
-      <Drawer show={isOpen} onHide={closeModal} placement="left">
+      <Drawer
+        full={isMobileDevice}
+        show={isOpen}
+        onHide={closeModal}
+        placement="left"
+      >
         <Dashboard />
       </Drawer>
     </>
