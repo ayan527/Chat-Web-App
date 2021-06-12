e30fe304674f6a4ad55915223a16e4b3dec2b434
Ayan Mukherjee <ayanmukherjee20102011@gmail.com>
2021-06-12 22:49:57 +0530

    started creating chat page. added routes to selected chat room

---
+++ src/pages/Home/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Switch, Route, useRouteMatch } from 'react-router';
+import { Col, Grid, Row } from 'rsuite';
+import Sidebar from '../../components/Sidebar';
+import { ChatRoomsProvider } from '../../context/rooms.context';
+import { useMediaQuery } from '../../misc/custom-hooks';
+import ChatPage from './ChatPage';
+
+const Home = () => {
+  const isDesktop = useMediaQuery('(min-width: 992px)');
+  const { isExact } = useRouteMatch();
+
+  const isRenderingSideBar = isDesktop || isExact;
+
+  return (
+    <ChatRoomsProvider>
+      <Grid fluid className="h-100">
+        <Row className="h-100">
+          {isRenderingSideBar && (
+            <Col xs={24} md={8} className="h-100">
+              <Sidebar />
+            </Col>
+          )}
+
+          <Switch>
+            <Route exact path="/chat/:chatId">
+              <Col xs={24} md={16} className="h-100">
+                <ChatPage />
+              </Col>
+            </Route>
+            <Route>
+              {isDesktop && (
+                <Col xs={24} md={16} className="h-100">
+                  <h6 className="text-center mt-page">
+                    Please select chat room
+                  </h6>
+                </Col>
+              )}
+            </Route>
+          </Switch>
+        </Row>
+      </Grid>
+    </ChatRoomsProvider>
+  );
+};
+
+export default Home;
