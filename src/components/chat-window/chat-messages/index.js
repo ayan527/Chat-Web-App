97f5f2a6461e284552d120d51ca9188b660094f9
Ayan Mukherjee <ayanmukherjee20102011@gmail.com>
2021-06-12 23:15:45 +0530

    chat page layout created

---
+++ src/components/chat-window/chat-messages/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ChatMessages = () => {
+  return <div>Messages</div>;
+};
+
+export default ChatMessages;
--- src/pages/Home/ChatPage.js
+++ src/pages/Home/ChatPage.js
@@ -1,7 +1,51 @@
 import React from 'react';
+import { Loader } from 'rsuite';
+import { useParams } from 'react-router';
+import ChatBottom from '../../components/chat-window/chat-bottom';
+import ChatMessages from '../../components/chat-window/chat-messages';
+import ChatTop from '../../components/chat-window/chat-top';
+import { useRooms } from '../../context/rooms.context';
 
 const ChatPage = () => {
-  return <div>Hello</div>;
+  const { chatId } = useParams();
+
+  const rooms = useRooms();
+
+  if (!rooms) {
+    return (
+      <Loader
+        oader
+        center
+        vertical
+        backdrop
+        size="lg"
+        content="Loading..."
+        speed="slow"
+      />
+    );
+  }
+
+  const currentRoom = rooms.find(room => room.id === chatId);
+
+  if (!currentRoom) {
+    return (
+      <h6 className="text-center mt-page">Chat room: {chatId} not found</h6>
+    );
+  }
+
+  return (
+    <>
+      <div className="chat-top">
+        <ChatTop />
+      </div>
+      <div className="chat-middle">
+        <ChatMessages />
+      </div>
+      <div className="chat-bottom">
+        <ChatBottom />
+      </div>
+    </>
+  );
 };
 
 export default ChatPage;
