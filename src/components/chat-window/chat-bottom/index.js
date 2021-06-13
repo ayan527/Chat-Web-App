c3850cf0ccdbf57afa14fa8291fdf4542383129a
Ayan Mukherjee <ayanmukherjee20102011@gmail.com>
2021-06-13 11:17:54 +0530

    chat-bottom started creating. messages sent on click and on enter press.

--- src/components/chat-window/chat-bottom/index.js
+++ src/components/chat-window/chat-bottom/index.js
@@ -1,7 +1,92 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import firebase from 'firebase/app';
+import { Alert, Icon, InputGroup, Input } from 'rsuite';
+import { useParams } from 'react-router';
+import { useProfile } from '../../../context/profile.context';
+import { database } from '../../../misc/firebase';
+
+function assembleMessage(profile, chatId) {
+  return {
+    roomId: chatId,
+    author: {
+      name: profile.name,
+      uid: profile.uid,
+      createdAt: profile.createdAt,
+      ...(profile.picture ? { picture: profile.picture } : {}),
+    },
+    createdAt: firebase.database.ServerValue.TIMESTAMP,
+  };
+}
 
 const ChatBottom = () => {
-  return <div>bottom</div>;
+  const [inputText, setInputText] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { chatId } = useParams();
+  const { profile } = useProfile();
+
+  const onInputChange = useCallback(value => {
+    setInputText(value);
+  }, []);
+
+  const onSendClick = async () => {
+    if (inputText.trim() === '') {
+      return;
+    }
+
+    const msgData = assembleMessage(profile, chatId);
+    msgData.text = inputText;
+
+    const updates = {};
+
+    const messageId = database.ref('messages').push().key;
+
+    updates[`/messages/${messageId}`] = msgData;
+    updates[`/rooms/${chatId}/lastMessage`] = {
+      ...msgData,
+      msgId: messageId,
+    };
+
+    setIsLoading(true);
+    try {
+      await database.ref().update(updates);
+
+      setInputText('');
+      setIsLoading(false);
+    } catch (err) {
+      setIsLoading(false);
+      Alert.error(err.message, 4000);
+    }
+  };
+
+  const onPressEnter = ev => {
+    if (ev.keyCode === 13) {
+      ev.preventDefault();
+      onSendClick();
+    }
+  };
+
+  return (
+    <div>
+      <InputGroup>
+        <Input
+          placeholder="Write a new message here..."
+          value={inputText}
+          onChange={onInputChange}
+          onKeyDown={onPressEnter}
+        />
+
+        <InputGroup.Button
+          color="blue"
+          appearance="primary"
+          onClick={onSendClick}
+          disabled={isLoading}
+        >
+          <Icon icon="send" />
+        </InputGroup.Button>
+      </InputGroup>
+    </div>
+  );
 };
 
 export default ChatBottom;
